c6ec8633994a05fc9c2bd1d1cb8206de65fd6dcb
Roger Terrill <lpaddikt@gmail.com>
2020-09-16 14:46:05 -0700

    Added first modal for add Campus

--- src/components/Admin/AdminPage.js
+++ src/components/Admin/AdminPage.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { compose } from 'recompose';
 import { withAuthorization, withEmailVerification } from '../Session';
 import * as ROLES from '../../constants/roles';
 import * as ROUTES from '../../constants/routes';
 
-import UserList from './UserList';
-import UserItem from './UserItem';
+import { UserList, UserItem } from '../User';
 import AddCampusForm from '../Campus';
 import AddMajorForm from '../Major';
 import AddCourseForm from '../Course';
 
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 const AdminPage = () => {
+  const [campusModalShow, setCampusModalShow] = useState(false);
+
   return (
     <>
       <h1>Admin</h1>
       <p>The Admin Page is accessible by every signed in admin user.</p>
+      <Button variant='primary' onClick={() => setCampusModalShow(true)}>
+        Add Campus
+      </Button>
+      <Modal
+        onHide={() => setCampusModalShow(false)}
+        show={campusModalShow}
+        size='lg'
+        animation={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id='contained-modal-title-vcenter'>
+            Add Campus
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddCampusForm />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setCampusModalShow(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
       <Switch>
         <Route exact path={ROUTES.ADMIN_USERS_DETAILS} component={UserItem} />
         <Route exact path={ROUTES.ADMIN} component={UserList} />
---
+++ src/components/User/index.js
@@ -0,0 +1,6 @@
+import User from './User';
+import UserItem from './UserItem';
+import UserList from './UserList';
+
+export { UserList, UserItem };
+export default User;
