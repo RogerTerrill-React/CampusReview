1c8d3c241bf741b48d4dc105976282d84c150e3d
Roger Terrill <rogert@channelbakers.com>
2020-10-04 16:46:02 -0700

    Added offered by campus

--- src/components/Course/CourseInfo.js
+++ src/components/Course/CourseInfo.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import {useCampusList} from '../Campus';
 
 const CourseInfo = ({ course }) => {
+  const campusList = useCampusList();
+  const campus = campusList.filter((campus) => campus.uid === course.schoolId)[0];
   return (
     <Card>
       <Card.Header as="h5" className="text-center">
@@ -10,6 +13,8 @@ const CourseInfo = ({ course }) => {
       <Card.Body>
         <Card.Title>About</Card.Title>
         <Card.Text>{course.description}</Card.Text>
+        <hr />
+        <Card.Text>Offered at: {campus && campus.name}</Card.Text>
       </Card.Body>
     </Card>
   );
