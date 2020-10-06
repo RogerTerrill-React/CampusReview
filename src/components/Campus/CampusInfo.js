46f72ab229c301858ee4cc85ca0173ca0b2e0cac
Roger Terrill <rogert@channelbakers.com>
2020-10-05 20:43:33 -0700

    Formatting

--- src/components/Campus/CampusInfo.js
+++ src/components/Campus/CampusInfo.js
@@ -1,40 +1,36 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const CampusInfo = ({ campus }) => {
-  console.log(campus);
   return (
     <Card>
-      <Card.Header as="h5" className='text-center'>General Information</Card.Header>
-      <Card.Img variant="top" className='p-4' src={campus.logoUrl} />
+      <Card.Header as='h5' className='text-center'>
+        General Information
+      </Card.Header>
+      <Card.Img variant='top' className='p-4' src={campus.logoUrl} />
       <Card.Body>
         <Card.Title>About</Card.Title>
-        <Card.Text>
-          {campus.about}
-        </Card.Text>
+        <Card.Text>{campus.about}</Card.Text>
       </Card.Body>
       <Card.Body>
         <Card.Title>Website</Card.Title>
         <Card.Text>
-        <a target="_blank" rel="noopener noreferrer" href={campus.url}>{campus.url}</a>
+          <a target='_blank' rel='noopener noreferrer' href={campus.url}>
+            {campus.url}
+          </a>
         </Card.Text>
       </Card.Body>
-      
+
       <Card.Body>
         <Card.Title>Address</Card.Title>
-        <Card.Text className='mb-0'>
-          {campus.address}
-        </Card.Text>
+        <Card.Text className='mb-0'>{campus.address}</Card.Text>
         <Card.Text className='mb-0'>
           {campus.city}, {campus.state} {campus.zipcode}
         </Card.Text>
-        <Card.Text className='mb-0'>
-          {campus.phoneNumber}
-        </Card.Text>
+        <Card.Text className='mb-0'>{campus.phoneNumber}</Card.Text>
       </Card.Body>
-
     </Card>
-  )
-}
+  );
+};
 
-export default CampusInfo
+export default CampusInfo;
--- src/components/Course/CourseReviews.js
+++ src/components/Course/CourseReviews.js
@@ -1,71 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import { useCourseList } from '../Course';
-import { useAuthUser } from "../Session";
+import { useAuthUser } from '../Session';
 import AddCourseReviewModal from './AddCourseReviewModal';
-import {CourseReviewsList} from './CourseList';
+import { CourseReviewsList } from './CourseList';
 import styled from 'styled-components';
 import Card from 'react-bootstrap/Card';
 
 const CourseReviews = ({ course, setRatings }) => {
   const courseList = useCourseList();
   const authUser = useAuthUser();
-  
-  const INITIAL_STATE ={
+
+  const INITIAL_STATE = {
     average: 0,
     count: 0,
-  }
+  };
 
   const [values, setValues] = useState(INITIAL_STATE);
   const [reviews, setReviews] = useState([]);
   const [ratingsArray, setRatingsArray] = useState([]);
 
   useEffect(() => {
-    
     // Filter list to only those that have reviews and match the course
-    const courseWithReviews = courseList.filter(courseElement => {
+    const courseWithReviews = courseList.filter((courseElement) => {
       return courseElement.reviews && course.uid === courseElement.uid;
     });
-    
-    if(courseWithReviews[0]){
 
+    if (courseWithReviews[0]) {
       // Add the uid to each review object
-      const reviewsList = Object.keys(courseWithReviews[0].reviews).map((key) => ({
-        ...courseWithReviews[0].reviews[key],
-        uid: key,
-      }));
-      console.log(reviewsList)
+      const reviewsList = Object.keys(courseWithReviews[0].reviews).map(
+        (key) => ({
+          ...courseWithReviews[0].reviews[key],
+          uid: key,
+        })
+      );
       setReviews(reviewsList);
-      const scoreArray = reviewsList.map(review => review.score);
-      const length = scoreArray.length ? scoreArray.length : 1
-      const total = scoreArray.reduce(((score, sum) => score + sum),0)
+      const scoreArray = reviewsList.map((review) => review.score);
+      const length = scoreArray.length ? scoreArray.length : 1;
+      const total = scoreArray.reduce((score, sum) => score + sum, 0);
       const average = total / length;
-      setRatings({reviewCount: length, averageScore: average});
-      setValues({...values, average, count: scoreArray.length});
+      setRatings({ reviewCount: length, averageScore: average });
+      setValues({ ...values, average, count: scoreArray.length });
       setRatingsArray(scoreArray);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [courseList.length])
+  }, [courseList.length]);
 
   return (
     <>
       <Card>
-        <Card.Header as="h5" className='text-center position-relative'>
-           Recent Reviews {authUser && <AddCourseReviewModal
-                course={course}
-                ratingsArray={ratingsArray}
-              />}
+        <Card.Header as='h5' className='text-center position-relative'>
+          Recent Reviews{' '}
+          {authUser && (
+            <AddCourseReviewModal course={course} ratingsArray={ratingsArray} />
+          )}
         </Card.Header>
         <ReviewsBox>
           {reviews && <CourseReviewsList reviews={reviews} />}
         </ReviewsBox>
       </Card>
     </>
-  )
-}
+  );
+};
 
 export default CourseReviews;
 
 const ReviewsBox = styled.div`
   max-height: 49rem;
   overflow-y: auto;
-`
+`;
